237433e9d38b533f199b85f364c479af66a271b9
rhill34 <rhill34@mail.greenriver.edu>
2019-12-28 09:51:14 -0600

    :hammer: Handle 404s. @rhill34

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom"; import Home from "./containers/Home";
+import { Route, Switch } from "react-router-dom"; 
+import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
 
 // This component uses this Switch component from React-Router that renders the first matching route that is defined within it. For now we only have a single route, it looks for / and renders the Home component when matched. We are also using the exact prop to ensure that it matches the / route exactly. This is because the path / will also match any route that starts with a /.
 
@@ -7,5 +9,7 @@ export default function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={Home} />
+            { /* Finally, catch all unmatched routes */ } 
+            <Route component={NotFound} />
         </Switch>);
 }
