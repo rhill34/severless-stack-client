9bbad458110902bd82d55e61ef141d296b295dc8
rhill34 <rhill34@mail.greenriver.edu>
2019-12-30 12:02:53 -0600

    :books: POST a new note/content. @rhill34

---
+++ src/containers/NewNote.js
@@ -0,0 +1,9 @@
+/**
+Now that we can signup users and also log them in. Let’s get started with the most important part of our note taking app; the crea:on of a note.
+
+First we are going to create the form for a note. It’ll take some content and a file as an a>achment.
+
+**This Part is the Challenge to get the user to be able to record / upload a from the there device up to a 30 second video file and  a PDF file for the resume or content. 
+
+--Remember Mini Glory 
+*/
