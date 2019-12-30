3e661a1701b3c471d02f56c2f9fd5b990a6273fa
rhill34 <rhill34@mail.greenriver.edu>
2019-12-30 10:15:49 -0600

    :sparkles: ADDED signup form to the REACT APP. @rhill34

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import AppliedRoute from "./components/AppliedRoute";
 import ResetPassword from "./containers/ResetPassword";
+import Signup from "./containers/Signup";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 // This component uses this Switch component from React-Router that renders the first matching route that is defined within it. For now we only have a single route, it looks for / and renders the Home component when matched. We are also using the exact prop to ensure that it matches the / route exactly. This is because the path / will also match any route that starts with a /.
 
@@ -19,6 +20,7 @@ export default function Routes({ appProps, childProps }) {
                 component={ResetPassword}
                 props={childProps}
             />
+             <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
             { /* Finally, catch all unmatched routes */}
             <Route component={NotFound} />
         </Switch>
