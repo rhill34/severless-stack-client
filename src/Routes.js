fc112a8f9a252285465fecafadeca3c19157fffb
rhill34 <rhill34@mail.greenriver.edu>
2019-12-30 11:58:11 -0600

    :sparkles: Signup Created and Functioning. @rhill323
    Still need to handle IsAuthenticated error in the settings for functionality testing.

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import AppliedRoute from "./components/AppliedRoute";
 import ResetPassword from "./containers/ResetPassword";
 import Signup from "./containers/Signup";
-import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+import Settings from "./containers/Settings";
+import ChangePassword from "./containers/ChangePassword";
+
 // This component uses this Switch component from React-Router that renders the first matching route that is defined within it. For now we only have a single route, it looks for / and renders the Home component when matched. We are also using the exact prop to ensure that it matches the / route exactly. This is because the path / will also match any route that starts with a /.
 
 export default function Routes({ appProps, childProps }) {
@@ -14,13 +19,25 @@ export default function Routes({ appProps, childProps }) {
         <Switch>
             <AppliedRoute path="/" exact component={Home} appProps={appProps} />
             <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+            <AuthenticatedRoute
+                path="/settings"
+                exact
+                component={Settings}
+                props={childProps}
+            />
+            <AuthenticatedRoute
+                path="/settings/password"
+                exact
+                component={ChangePassword}
+                props={childProps}
+            />
             <UnauthenticatedRoute
                 path="/login/reset"
                 exact
                 component={ResetPassword}
                 props={childProps}
             />
-             <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+            <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
             { /* Finally, catch all unmatched routes */}
             <Route component={NotFound} />
         </Switch>
