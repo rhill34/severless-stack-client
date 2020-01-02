93a1a4acc6a3153f1f97bb11921e07058685073b
rhill34 <rhill34@mail.greenriver.edu>
2020-01-02 17:48:28 -0600

    :sparkles: User can upload a file. @rhill34

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -7,161 +7,175 @@ import config from "../config";
 import "./Notes.css";
 
 export default function Notes(props) {
-  const file = useRef(null);
-  const [note, setNote] = useState(null);
-  const [content, setContent] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false);
+    const file = useRef(null);
+    const [note, setNote] = useState(null);
+    const [content, setContent] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
 
-  useEffect(() => {
-    function loadNote() {
-      return API.get("notes", `/notes/${props.match.params.id}`);
-    }
-
-    async function onLoad() {
-      try {
-        const note = await loadNote();
-        const { content, attachment } = note;
-
-        if (attachment) {
-          note.attachmentURL = await Storage.vault.get(attachment);
+    useEffect(() => {
+        function loadNote() {
+            return API.get("notes", `/notes/${props.match.params.id}`);
         }
 
-        setContent(content);
-        setNote(note);
-      } catch (e) {
-        alert(e);
-      }
+        async function onLoad() {
+            try {
+                const note = await loadNote();
+                const { content, attachment } = note;
+
+                if (attachment) {
+                    note.attachmentURL = await Storage.vault.get(attachment);
+                }
+
+                setContent(content);
+                setNote(note);
+            } catch (e) {
+                alert(e);
+            }
+        }
+
+        onLoad();
+    }, [props.match.params.id]);
+
+    function validateForm() {
+        return content.length > 0;
     }
 
-    onLoad();
-  }, [props.match.params.id]);
-
-  function validateForm() {
-    return content.length > 0;
-  }
-
-  function formatFilename(str) {
-    return str.replace(/^\w+-/, "");
-  }
-
-  function handleFileChange(event) {
-    file.current = event.target.files[0];
-  }
-
-  function saveNote(note) {
-    return API.put("notes", `/notes/${props.match.params.id}`, {
-      body: note
-    });
-  }
-
-  async function handleSubmit(event) {
-    let attachment;
-
-    event.preventDefault();
-
-    if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
-      alert(
-        `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
-          1000000} MB.`
-      );
-      return;
+    function formatFilename(str) {
+        return str.replace(/^\w+-/, "");
     }
 
-    setIsLoading(true);
-
-    try {
-      if (file.current) {
-        attachment = await s3Upload(file.current);
-      }
-
-      await saveNote({
-        content,
-        attachment: attachment || note.attachment
-      });
-      props.history.push("/");
-    } catch (e) {
-      alert(e);
-      setIsLoading(false);
+    function handleFileChange(event) {
+        file.current = event.target.files[0];
     }
-  }
 
-  function deleteNote() {
-    return API.del("notes", `/notes/${props.match.params.id}`);
-  }
+    function saveNote(note) {
+        return API.put("notes", `/notes/${props.match.params.id}`, {
+            body: note
+        });
+    }
 
-  async function handleDelete(event) {
-    event.preventDefault();
+    function saveNote(note) {
+        return API.put("notes", `/notes/${props.match.params.id}`, {
+            body: note
+        });
+    }
 
-    const confirmed = window.confirm(
-      "Are you sure you want to delete this note?"
+    /**
+     * If there is a file to upload we call s3Upload to upload it and save the key we get from S3. If there isn’t then we simply save the existing attachment object, note.attachment.
+
+    We save the note by making a PUT request with the note object to /notes/:id where we get the id from props.match.params.id. We use the API.put() method from AWS Amplify.
+
+    And on success we redirect the user to the homepage.
+     * @param {*} event 
+     */
+    async function handleSubmit(event) {
+        let attachment;
+
+        event.preventDefault();
+
+        if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
+            alert(
+                `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
+                1000000} MB.`
+            );
+            return;
+        }
+
+        setIsLoading(true);
+
+        try {
+            if (file.current) {
+                attachment = await s3Upload(file.current);
+            }
+
+            await saveNote({
+                content,
+                attachment: attachment || note.attachment
+            });
+            props.history.push("/");
+        } catch (e) {
+            alert(e);
+            setIsLoading(false);
+        }
+    }
+
+    function deleteNote() {
+        return API.del("notes", `/notes/${props.match.params.id}`);
+    }
+
+    async function handleDelete(event) {
+        event.preventDefault();
+
+        const confirmed = window.confirm(
+            "Are you sure you want to delete this note?"
+        );
+
+        if (!confirmed) {
+            return;
+        }
+
+        setIsDeleting(true);
+
+        try {
+            await deleteNote();
+            props.history.push("/");
+        } catch (e) {
+            alert(e);
+            setIsDeleting(false);
+        }
+    }
+
+    return (
+        <div className="Notes">
+            {note && (
+                <form onSubmit={handleSubmit}>
+                    <FormGroup controlId="content">
+                        <FormControl
+                            value={content}
+                            componentClass="textarea"
+                            onChange={e => setContent(e.target.value)}
+                        />
+                    </FormGroup>
+                    {note.attachment && (
+                        <FormGroup>
+                            <ControlLabel>Attachment</ControlLabel>
+                            <FormControl.Static>
+                                <a
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    href={note.attachmentURL}
+                                >
+                                    {formatFilename(note.attachment)}
+                                </a>
+                            </FormControl.Static>
+                        </FormGroup>
+                    )}
+                    <FormGroup controlId="file">
+                        {!note.attachment && <ControlLabel>Attachment</ControlLabel>}
+                        <FormControl onChange={handleFileChange} type="file" />
+                    </FormGroup>
+                    <LoaderButton
+                        block
+                        type="submit"
+                        bsSize="large"
+                        bsStyle="primary"
+                        isLoading={isLoading}
+                        disabled={!validateForm()}
+                    >
+                        Save
+          </LoaderButton>
+                    <LoaderButton
+                        block
+                        bsSize="large"
+                        bsStyle="danger"
+                        onClick={handleDelete}
+                        isLoading={isDeleting}
+                    >
+                        Delete
+          </LoaderButton>
+                </form>
+            )}
+        </div>
     );
-
-    if (!confirmed) {
-      return;
-    }
-
-    setIsDeleting(true);
-
-    try {
-      await deleteNote();
-      props.history.push("/");
-    } catch (e) {
-      alert(e);
-      setIsDeleting(false);
-    }
-  }
-
-  return (
-    <div className="Notes">
-      {note && (
-        <form onSubmit={handleSubmit}>
-          <FormGroup controlId="content">
-            <FormControl
-              value={content}
-              componentClass="textarea"
-              onChange={e => setContent(e.target.value)}
-            />
-          </FormGroup>
-          {note.attachment && (
-            <FormGroup>
-              <ControlLabel>Attachment</ControlLabel>
-              <FormControl.Static>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={note.attachmentURL}
-                >
-                  {formatFilename(note.attachment)}
-                </a>
-              </FormControl.Static>
-            </FormGroup>
-          )}
-          <FormGroup controlId="file">
-            {!note.attachment && <ControlLabel>Attachment</ControlLabel>}
-            <FormControl onChange={handleFileChange} type="file" />
-          </FormGroup>
-          <LoaderButton
-            block
-            type="submit"
-            bsSize="large"
-            bsStyle="primary"
-            isLoading={isLoading}
-            disabled={!validateForm()}
-          >
-            Save
-          </LoaderButton>
-          <LoaderButton
-            block
-            bsSize="large"
-            bsStyle="danger"
-            onClick={handleDelete}
-            isLoading={isDeleting}
-          >
-            Delete
-          </LoaderButton>
-        </form>
-      )}
-    </div>
-  );
 }
